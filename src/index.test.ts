5b72c4738280ab6beaed90ebf40c668f15d4027b
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2023-12-02 15:02:37 +0600

    Modified the test data

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
 import { Plugin, PluginMeta } from "@posthog/plugin-scaffold";
 // @ts-ignore
-import { createPageview, resetMeta, createIdentify } from "@posthog/plugin-scaffold/test/utils";
+import { resetMeta } from "@posthog/plugin-scaffold/test/utils";
 
 import * as index from "./index";
 import type { PluginConfig } from "./types";
 
-const { processEvent, setupPlugin } = index as Required<Plugin>;
+const { setupPlugin } = index as Required<Plugin>;
 
 const defaultConfig: PluginConfig = {
   octolaneApiKey: "KEY",
@@ -57,33 +57,6 @@ describe("OctoLane Plugin", () => {
           octolaneApiKey: OVERRIDDEN_API_KEY,
         }),
       );
-    });
-  });
-
-  describe("processEvent()", () => {
-    const MOCK_IP = "89.160.20.129";
-    const MOCK_UUID = "37114ebb-7b13-4301-b849-0d0bd4d5c7e5";
-    const MOCK_EMAIL = "test@posthog.com";
-
-    it("process event with mock ip for pageview", async () => {
-      const event = await processEvent(
-        { ...createPageview(), ip: MOCK_IP, uuid: MOCK_UUID },
-        await resetMetaWithGeoIp(),
-      );
-
-      expect(event?.event).toEqual("$pageview");
-      expect(event?.ip).toEqual(MOCK_IP);
-    });
-
-    it("process event with mock ip for identify", async () => {
-      const event = await processEvent(
-        { ...createIdentify(), ip: MOCK_IP, uuid: MOCK_UUID },
-        await resetMetaWithGeoIp(),
-      );
-
-      expect(event?.event).toEqual("$identify");
-      expect(event?.ip).toEqual(MOCK_IP);
-      expect(event?.$set).toEqual(expect.objectContaining({ email: MOCK_EMAIL }));
     });
   });
 });
